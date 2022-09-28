21c4c4aefc51635e9a932b18818a667d351662f1
aspardog <92935386+aspardog@users.noreply.github.com>
2022-09-28 12:59:58 -0500

    Update Loading
    Update loading with mirror chart and slope chart

--- loading.R
+++ loading.R
@@ -1,2 +1,4 @@
 source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/line_chart.R")
 source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/horizontal_bars_chart.R")
+source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/mirror_chart.R")
+source("https://github.com/ctoruno/WJP-Data-Viz/blob/main/slope_chart.R")
