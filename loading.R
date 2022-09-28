2c06f6af4726242d05577d94fd3db90d36794f3b
Carlos A. Toruño P <carlos.toruno@gmail.com>
2022-09-28 16:12:03 -0500

    Update loading.R

--- loading.R
+++ loading.R
@@ -2,3 +2,4 @@ source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/line_chart.R
 source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/horizontal_bars_chart.R")
 source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/mirror_chart.R")
 source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/slope_chart.R")
+source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/dots%26errors_chart.R")
