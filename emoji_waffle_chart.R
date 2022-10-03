e5379ad9a859f9c47522bfc0651e919e14697dcf
aspardog <92935386+aspardog@users.noreply.github.com>
2022-10-03 16:44:08 -0500

    New charts

---
+++ emoji_waffle_chart.R
@@ -0,0 +1,19 @@
+emoji_waffle_chart <- function(data2plot, title_label, subtitle_label, caption_label, main_color){
+  parts=c("Yes"= (data2plot$Yes)*100, "No"= (data2plot$No)*100)
+  names(parts) = paste0(names(parts)," ","="," ",parts,"%")
+  
+  waffle(parts,
+         rows = 10, colors = c(main_color, "#dee2cc"),
+         legend_pos="bottom",
+         use_glyph = 'child',
+         glyph_size = 3,
+         title = title_label) +
+    labs(subtitle = subtitle_label,
+         caption = caption_label) +
+    theme(plot.subtitle= element_text(family = "Lato Light Italic", size=6, hjust = 0.5,color="Black", margin = unit(c(0,0,-3,0), "cm")),
+          plot.title= element_text(family="Lato Black",size=10,hjust = 0.5, color="Black", margin = unit(c(0,0,-1,0), "cm")),
+          plot.caption = element_text(family="Lato Regular",size=6,hjust = 0.5,color="Black"),
+          plot.title.position = "plot",
+          legend.position = "bottom")
+          
+  }
---
+++ horizontal_edgebars_chart.R
@@ -0,0 +1,39 @@
+horizontal_edgebars_chart <- function(data2plot, title_label, subtitle_label, main_color) {
+  
+  plot <- ggplot(data2plot)+ aes(x = target_value, y = reorder(target, order_value), fill = pos) +
+    geom_bar(position="stack", stat="identity", width=0.60, color = main_color) +
+    scale_fill_manual(values = c("white", main_color)) +
+    labs(title = title_label,
+         subtitle = subtitle_label,
+         x = "",
+         y = "")  +
+    geom_text(aes(label = label_value, x = 0.5), check_overlap = T, color = main_color, size = 2.5) + 
+    coord_cartesian(xlim = c(0, 1), clip = "off") +
+    scale_x_continuous(labels = scales::percent_format(accuracy = 1),
+                       limits = c(0, 1),
+                       breaks = seq(0, 1, by = 0.2),
+                       expand=c(0, 0)) +
+    theme_minimal()+
+    theme(legend.position="none",
+          legend.text = element_text(family = "Lato Light", size = 4),
+          panel.grid.major.y = element_blank(),
+          panel.grid.minor.x = element_blank(),
+          axis.title.y=element_blank(),
+          axis.ticks.y=element_blank(),
+          axis.text.x=element_blank(),
+          axis.text.y=element_text(family = "Lato Full",
+                                   face = "plain",
+                                   size = 6), 
+          plot.background = element_rect(fill = "white", colour = "white"),
+          plot.subtitle=element_text(family = "Lato Full", 
+                                     face   = "italic",
+                                     size   = 7, 
+                                     vjust  = 0, 
+                                     color  = "Black", 
+                                     margin = margin(0,0,0,0)),
+          plot.title=element_text(family="Lato Black", size=10, hjust = -0.0,color="Black", margin=margin(0,0,10,0)),
+          plot.title.position = "plot",
+          panel.grid.major.x = element_blank())
+  
+  return(plot)
+}
