de1cbf673b55cb857b840b7e5b5db177eefce4fa
aspardog <92935386+aspardog@users.noreply.github.com>
2022-10-03 18:44:10 -0500

    Update emoji_waffle_chart.R

--- emoji_waffle_chart.R
+++ emoji_waffle_chart.R
@@ -12,9 +12,9 @@ emoji_waffle_chart <- function(data2plot, title_label, subtitle_label, caption_l
          caption = caption_label) +
     theme(plot.subtitle= element_text(family = "Lato Light Italic", size=6, hjust = 0.5,color="Black", margin = unit(c(0,0,-3,0), "cm")),
           plot.title= element_text(family="Lato Black",size=10,hjust = 0.5, color="Black", margin = unit(c(0,0,-1,0), "cm")),
-          plot.caption = element_text(family="Lato Regular",size=6,hjust = 0.5,color="Black"),
+          plot.caption = element_text(family="Lato Regular", size=6, hjust = 0.5, color="Black"),
           plot.title.position = "plot",
-          legend.text = element_text(family="Lato Regular",size=6,hjust = 0.5,color="Black"),
+          legend.text = element_text(family="Lato Regular", size=6, hjust = 0.5, color="Black"),
           legend.box.spacing = unit(0, "pt"),
           legend.margin=margin(0,0,0,0),
           legend.box.margin=margin(-50,0,0,0)) 
