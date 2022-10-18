8499ccda56208c5534437e28934389f82b50de3a
aspardog <92935386+aspardog@users.noreply.github.com>
2022-10-18 10:23:54 -0500

    Update loading.R

--- loading.R
+++ loading.R
@@ -6,3 +6,4 @@ source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/slope_chart.
 source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/dots%26errors_chart.R")
 source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/horizontal_edgebars_chart.R")
 source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/emoji_waffle_chart.R")
+source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/logit_plot_3panels.R")
