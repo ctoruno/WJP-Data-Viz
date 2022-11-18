5a30a91d190484784e0773a806394f8c2acc7b92
Carlos A. Toruño P <carlos.toruno@gmail.com>
2022-11-18 13:59:24 -0600

    Create divBars.R

---
+++ LAC/divBars.R
@@ -0,0 +1,65 @@
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##
+## Script:            LAC Reports Data Viz functions: Diverging Horizontal Bars Chart
+##
+## Author(s):         Carlos A. Toruño Paniagua   (ctoruno@worldjusticeproject.org)
+##
+## Dependencies:      World Justice Project
+##
+## Creation date:     November 18th, 2022
+##
+## This version:      November 18th, 2022
+##
+## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+LAC_divBars <- function(
+    data,             # Data frame with data
+    target_var,       # Variable that will supply the values to plot
+    grouping_var,     # Variable containing the grouping values (Y-Axis Labels)
+    diverging_var,    # Variable that contains the values to diverge,
+    negative_value,   # Negative value showwed in the diverging_var
+    colors,           # Colors to apply to diverging bar
+    labels_var        # Variable containing the labels to show in the plot
+){
+  
+  # Renaming variabless in the data frame to match the function naming
+  data <- data %>%
+    rename(target_var    = all_of(target_var),
+           grouping_var  = all_of(grouping_var),
+           diverging_var = all_of(diverging_var),
+           labels_var    = all_of(labels_var)) %>%
+    mutate(added_space = if_else(diverging_var == negative_value, -7.5, 7.5))
+  
+  # Creating ggplot
+  ggplot(data, aes(x     = grouping_var,
+                   y     = target_var,
+                   fill  = diverging_var,
+                   label = labels_var)) +
+    geom_bar(stat        = "identity",
+             position    = "stack",
+             show.legend = F) +
+    geom_text(aes(y = target_var + added_space),
+              size     = 10,
+              color    = "#4a4a49",
+              family   = "Lato Full",
+              fontface = "bold") +
+    geom_hline(yintercept = 0,
+               linetype   = "solid",
+               size       = 0.75,
+               color      = "#262424") + 
+    scale_fill_manual(values = binPalette) +
+    scale_y_continuous(limits = c(-100,100)) +
+    scale_x_discrete(limits   = rev) +
+    coord_flip() +
+    WJP_theme() +
+    theme(panel.grid.major = element_blank(),
+          axis.text.x      = element_blank(),
+          axis.text.y      = element_text(family = "Lato Full",
+                                          face   = "bold",
+                                          size   = 10*.pt,
+                                          color  = "#262424"),
+          axis.title.x      = element_blank(),
+          axis.title.y      = element_blank())
+    
+}
+
