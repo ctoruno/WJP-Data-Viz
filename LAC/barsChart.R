c4d1a83ef025f73aab389f25e7e8751701884aae
Carlos A. Toruño P <carlos.toruno@gmail.com>
2022-12-01 13:23:49 -0600

    Update Dec, 1st

--- LAC/barsChart.R
+++ LAC/barsChart.R
@@ -65,7 +65,9 @@ LAC_barsChart <- function(
       WJP_theme() +
       theme(panel.grid.major.y = element_blank(),
             panel.grid.major.x = element_line(color = "#D0D1D3"),
-            axis.title.y       = element_blank())
+            axis.title.y       = element_blank(),
+            axis.title.x       = element_blank(),
+            axis.text.y        = element_text(hjust = 0))
   }
     
   return(plt)
