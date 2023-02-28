977d6548a00d57d093496e45ae6cb8d8aa6cc418
Carlos A. Toruño P <carlos.toruno@gmail.com>
2023-02-28 16:33:20 -0600

    Update lineChart.R

--- LAC/lineChart.R
+++ LAC/lineChart.R
@@ -25,8 +25,9 @@ LAC_lineChart <- function(
     transparencies = NULL,   # Named vector with transparencies to apply
     custom.axis = F,         # Do we want to customize the X-AXIS?
     x.breaks    = NULL,      # Numeric vector with custom breaks for the X-Axis
-    x.labels    = NULL       # Character vector with labels for the x-axis. It has to be the same lenght than
-                             # x.breaks
+    x.labels    = NULL,      # Character vector with labels for the x-axis. It has to be the same lenght than
+                             # x.breaks,
+    sec.ticks   = NULL       # Numeric vector containing the minor breaks
 ){
   
   # Renaming variables in the data frame to match the function naming
@@ -102,26 +103,31 @@ LAC_lineChart <- function(
     plt <- plt +
       scale_color_manual(values = colors) +
       scale_y_continuous(limits = c(0, 105),
+                         expand = c(0,0),
                          breaks = seq(0,100,20),
-                         labels = paste0(seq(0,100,20), "%")) +
-      WJP_theme() +
-      theme(panel.grid.major.x = element_blank(),
-            panel.grid.major.y = element_line(colour = "#d1cfd1"),
-            axis.title.x       = element_blank(),
-            axis.title.y       = element_blank())
+                         labels = paste0(seq(0,100,20), "%"))
   } else {
     plt <- plt +
       scale_color_manual(values = colors) +
       scale_y_continuous(limits = c(0, 105),
+                         expand = c(0,0),
                          breaks = seq(0,100,20),
                          labels = paste0(seq(0,100,20), "%")) +
       scale_x_continuous(breaks = x.breaks,
-                         labels = x.labels) +
-      WJP_theme() +
-      theme(panel.grid.major.x = element_blank(),
-            panel.grid.major.y = element_line(colour = "#d1cfd1"),
-            axis.title.x       = element_blank(),
-            axis.title.y       = element_blank())
+                         labels = x.labels,
+                         guide = "axis_minor",
+                         minor_breaks = sec.ticks)
   }
+  
+  plt <- plt +
+    WJP_theme() +
+    theme(panel.grid.major.x = element_blank(),
+          panel.grid.major.y = element_line(colour = "#d1cfd1"),
+          axis.title.x       = element_blank(),
+          axis.title.y       = element_blank(),
+          axis.line.x        = element_line(color    = "#d1cfd1"),
+          axis.ticks.x       = element_line(color    = "#d1cfd1",
+                                            linetype = "solid"),
+          ggh4x.axis.ticks.length.minor = rel(1))
   
 }
