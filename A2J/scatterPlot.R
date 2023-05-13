3884fd6a6f1519c33ad90fe6666e493da1e16dec
aspardog <92935386+aspardog@users.noreply.github.com>
2023-05-13 11:33:07 -0500

    Create scatterPlot.R

---
+++ A2J/scatterPlot.R
@@ -0,0 +1,30 @@
+scatterPlot <- function(data2plot = data2plot,
+                      xvariable  = Incidence,
+                      yvariable = Severity,
+                      category   = problem,
+                      colors4plot = colors4plot
+                      ) {
+  ggplot() + 
+  geom_point(data = data2plot,
+             aes(y = {{yvariable}}, 
+                 x = {{xvariable}},
+                 color = {{category}}),
+             show.legend = F) + # Show dots
+  geom_smooth(data = data2plot,
+              aes(y = {{yvariable}},
+                  x = {{xvariable}}),
+              method = "lm", se = T, color = "black", linetype="dashed", size = 1) +
+  labs(x = "Average Incidence",
+       y = "Average Severity") + 
+  scale_y_continuous(limits = c(0, 10.5),
+                     breaks = seq(0,10,2),
+                     labels = paste0(seq(0,10,2)),
+                     position = "left", expand = c(0,0)) +
+  scale_x_continuous(limits = c(0, 27),
+                     breaks = seq(0,25,5),
+                     labels = paste0(seq(0,25,5), "%"),
+                     position = "bottom", expand = c(0,0)) +
+  scale_color_manual(values = colors4plot) +
+  WJP_theme() +
+  theme(axis.line        = element_line(color    = "#5e5c5a", linetype = "solid"))
+}
