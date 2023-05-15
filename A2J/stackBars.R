c9351c0d0465c2ace6b7da13ddc26f9a1b622029
aspardog <92935386+aspardog@users.noreply.github.com>
2023-05-15 11:56:26 -0500

    Update stackBars.R

--- A2J/stackBars.R
+++ A2J/stackBars.R
@@ -10,7 +10,7 @@ stackBars <- function(data2plot = data2plot,
   
   ggplot() + 
     geom_col(data = data2plot,
-             aes(x = reorder({{country}}, {{-order_values}}), y = {{values}}, fill = {{category}}), 
+             aes(x = reorder({{country}}, -{{order_values}}), y = {{values}}, fill = {{category}}), 
              width = 0.5,
              show.legend = F) +
     scale_y_continuous(limits = c(0, 130),
@@ -20,7 +20,7 @@ stackBars <- function(data2plot = data2plot,
                        expand = c(0,0)) +
     scale_fill_manual(values = colors4plot) +
     geom_text(data = data2text, 
-              aes(y    = 120, x = reorder({{country}}, {{-order_values}}), label = {{label_value}}),
+              aes(y    = 120, x = reorder({{country}}, -{{order_values}}), label = {{label_value}}),
               color    = "#222221",
               family   = "Lato Full",
               fontface = "bold", size = 2.811678) +
@@ -30,12 +30,11 @@ stackBars <- function(data2plot = data2plot,
           panel.grid.major.x = element_line(color = c(rep("#5e5c5a", 6), "white")),
           axis.title.y       = element_blank(),
           axis.title.x       = element_blank(),
-          axis.text.y        = element_text(hjust = 0),
           axis.line.y        = element_line(color    = "#5e5c5a",
                                             linetype = "solid"),
           axis.text.y        = element_text(family   = "Lato Full",
                                             face     = "plain",
                                             size     = 2.811678*.pt,
-                                            color    = "#222221"))
+                                            color    = "#222221", hjust = 0))
   
 }
