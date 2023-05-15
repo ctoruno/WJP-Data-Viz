ceecf2032eaee0a1bcd8c7e35d240b3402e2abf7
aspardog <92935386+aspardog@users.noreply.github.com>
2023-05-15 12:39:41 -0500

    Update stackBars.R

--- A2J/stackBars.R
+++ A2J/stackBars.R
@@ -14,8 +14,9 @@ stackBars <- function(data2plot = data2plot,
              width = 0.5,
              show.legend = F) +
     scale_y_continuous(limits = c(0, 130),
-                       breaks = c(seq(0,100,20), 120),
-                       labels = c(paste0(seq(0,100,20), "%"), {{axis_label}}),
+                       breaks = c(seq(0,100,10), 120),
+                       labels = c("0%", " ", "20%", " ", "40%", " ", "60%", " ", "80%", " ", "100%", 
+                                  {{axis_label}}), 
                        position = "right", 
                        expand = c(0,0)) +
     scale_fill_manual(values = colors4plot) +
@@ -27,7 +28,7 @@ stackBars <- function(data2plot = data2plot,
     coord_flip()  +
     WJP_theme() +
     theme(panel.grid.major.y = element_blank(),
-          panel.grid.major.x = element_line(color = c(rep("#5e5c5a", 6), "white")),
+          panel.grid.major.x = element_line(color = c(rep("#5e5c5a", 11), "white")),
           axis.title.y       = element_blank(),
           axis.title.x       = element_blank(),
           axis.line.y        = element_line(color    = "#5e5c5a",
