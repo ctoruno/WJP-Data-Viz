1cc88ffdd14955ac58dddb5423634e261b4d5ff6
Artha Pillai <87118748+ArthaPillai@users.noreply.github.com>
2023-11-15 14:58:03 -0500

    Update loading.R

--- loading.R
+++ loading.R
@@ -47,5 +47,6 @@ loadVIZ <- function(set) {
     source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/LAC/radarChart.R")
     source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/LAC/lollipop_chart.R")
     source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/LAC/barsChart.R")
+    source("https://raw.githubusercontent.com/ctoruno/WJP-Data-Viz/main/LAC/logit_demo.R")
   }
 }
