53881746684975424ae78e4c9db3a85891649bef
Artha Pillai <87118748+ArthaPillai@users.noreply.github.com>
2023-11-28 14:37:20 -0500

    Create barsChart.R

---
+++ NM/barsChart.R
@@ -0,0 +1,114 @@
+NM_barsChart <- function(
+  data,                    # Data frame with data
+  target_var,              # Variable that will supply the values to plot
+  grouping_var,            # Variable containing the grouping values (Axis Labels)
+  labels_var,              # Variable containing the labels to show in the plot
+  colors_var,              # Variable containing the groups by color
+  colors,                  # Colors to apply to bars
+  repel = F,               # Do we need to repel the labels?
+  transparency = F,        # Apply transparency to char?
+  transparencies = NULL,   # Named vector with transparencies to apply
+  custom.axis = F,         # Do we want to customize the X-AXIS?
+  x.breaks    = NULL,      # Numeric vector with custom breaks for the X-Axis
+  x.labels    = NULL,      # Character vector with labels for the x-axis. It has to be the same length than
+  # x.breaks,
+  sec.ticks   = NULL       # Numeric vector containing the minor breaks
+) {
+  
+  # Renaming variables in the data frame to match the function naming
+  data <- data %>%
+    rename(target_var    = all_of(target_var),
+           grouping_var  = all_of(grouping_var),
+           labels_var    = all_of(labels_var),
+           colors_var     = all_of(colors_var))
+  
+  # Creating ggplot
+  plt <- ggplot(data,
+                aes(x     = grouping_var,
+                    y     = target_var,
+                    fill  = colors_var,
+                    label = labels_var))
+  
+  if (transparency == F) {
+    plt <- plt +
+      geom_bar(stat = "identity", position = "dodge", show.legend = F) +
+      geom_text(position = position_dodge(width = 0.9), 
+                vjust = -0.5, 
+                size = 3, 
+                show.legend = F)
+  } else {
+    plt <- plt +
+      geom_bar(stat = "identity", position = "dodge", aes(alpha   = colors_var), show.legend = F) +
+      geom_text(position = position_dodge(width = 0.9), 
+                vjust = -0.5, 
+                aes(alpha   = colors_var), 
+                size = 3, 
+                show.legend = F) +
+      scale_alpha_manual(values = transparencies)
+  }
+  
+  if (repel == F) {
+    # Applying regular geom_text
+    plt <- plt +
+      geom_text(aes(y     = target_var + 3,
+                    x     = grouping_var,
+                    label = labels_var),
+                family      = "Lato Full",
+                fontface    = "bold",
+                size        = 3.514598,
+                show.legend = F)
+  } else {
+    # Applying ggrepel for a better visualization of plots
+    plt <- plt +
+      geom_text_repel(mapping = aes(y     = target_var,
+                                    x     = grouping_var,
+                                    label = labels_var),
+                      family      = "Lato Full",
+                      fontface    = "bold",
+                      size        = 3.514598,
+                      show.legend = F,
+                      # Additional options from ggrepel package:
+                      min.segment.length = 1000,
+                      seed               = 42,
+                      box.padding        = 0.5,
+                      direction          = "y",
+                      force              = 5,
+                      force_pull         = 1)
+  }
+  
+  # Continuing with ggplot
+  
+  if (custom.axis == F) {
+    plt <- plt +
+      scale_fill_manual(values = colors) +
+      scale_y_continuous(limits = c(0, 105),
+                         expand = c(0,0),
+                         breaks = seq(0,100,20),
+                         labels = paste0(seq(0,100,20), "%"))
+  } else {
+    plt <- plt +
+      scale_fill_manual(values = colors) +
+      scale_y_continuous(limits = c(0, 105),
+                         expand = c(0,0),
+                         breaks = seq(0,100,20),
+                         labels = paste0(seq(0,100,20), "%")) +
+      scale_x_continuous(breaks = x.breaks,
+                         expand = expansion(mult = c(0.075, 0.125)),
+                         labels = x.labels,
+                         guide = "axis_minor",
+                         minor_breaks = sec.ticks)
+  }
+  
+  plt <- plt +
+    WJP_theme() +
+    theme(panel.grid.major.x = element_blank(),
+          panel.grid.major.y = element_line(colour = "#d1cfd1"),
+          axis.title.x       = element_blank(),
+          axis.title.y       = element_blank(),
+          axis.line.x        = element_line(color    = "#d1cfd1"),
+          axis.ticks.x       = element_line(color    = "#d1cfd1",
+                                            linetype = "solid"),
+          ggh4x.axis.ticks.length.minor = rel(1))
+  
+  return(plt)
+}
