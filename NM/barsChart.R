68715d06a562faa4438d0f45f4b04cefd66d1cef
Artha Pillai <87118748+ArthaPillai@users.noreply.github.com>
2023-12-04 13:46:25 -0500

    Update barsChart.R

--- NM/barsChart.R
+++ NM/barsChart.R
@@ -1,55 +1,52 @@
 LAC_groupedBarChart <- function(
-  data,                    # Data frame with data
-  target_var,              # Variable that will supply the values to plot
-  grouping_var,            # Variable containing the grouping values (Axis Labels)
-  ngroups,                 # Number of groups to plot
-  labels_var,              # Variable containing the labels to show in the plot
-  colors_var,              # Variable containing the groups by color
-  colors,                  # Colors to apply to bars
-  repel = F,               # Do we need to repel the labels?
-  transparency = F,        # Apply transparency to char?
-  transparencies = NULL,   # Named vector with transparencies to apply
-  custom.axis = F,         # Do we want to customize the X-AXIS?
-  x.breaks = NULL,         # Numeric vector with custom breaks for the X-Axis
-  x.labels = NULL          # Character vector with labels for the x-axis. It has to be the same length than x.breaks,
-) {
+    data,                    # Data frame with data
+    target_var,              # Variable that will supply the values to plot
+    grouping_var,            # Variable containing the grouping values (Axis Labels)
+    labels_var,              # Variable containing the labels to show in the plot
+    colors_var,              # Variable containing the groups by color
+    colors,                  # Colors to apply to bars
+    repel = FALSE,           # Do we need to repel the labels?
+    transparency = FALSE,    # Apply transparency to chart?
+    transparencies = NULL,   # Named vector with transparencies to apply
+    custom.axis = FALSE,     # Do we want to customize the X-AXIS?
+    x.breaks    = NULL,      # Numeric vector with custom breaks for the X-Axis
+    x.labels    = NULL       # Character vector with labels for the x-axis. It has to be the same length than x.breaks,
+){
   
   # Renaming variables in the data frame to match the function naming
   data <- data %>%
-    rename(
-      target_var = all_of(target_var),
-      grouping_var = all_of(grouping_var),
-      labels_var = all_of(labels_var),
-      colors_var = all_of(colors_var)
-    )
+    rename(target_var    = all_of(target_var),
+           grouping_var  = all_of(grouping_var),
+           labels_var    = all_of(labels_var),
+           colors_var     = all_of(colors_var))
   
   # Creating ggplot
-  plt <- ggplot(data,
-                aes(x = grouping_var,
-                    y = target_var,
-                    fill = colors_var,
+  plt <- ggplot(data, 
+                aes(x     = grouping_var,
+                    y     = target_var,
+                    fill  = colors_var,
                     label = labels_var))
-  
-  if (transparency == F) {
+    
+  if (transparency == FALSE) {
     plt <- plt +
       geom_bar(stat = "identity",
-               position = "dodge",  # Change to "dodge" for grouped bars
-               show.legend = F) +
+               position = "dodge",
+               show.legend = FALSE) +
       geom_text(position = position_dodge(width = 0.9),
                 vjust = -0.5,
                 size = 3.514598,
-                show.legend = F)
+                show.legend = FALSE)
   } else {
     plt <- plt +
-      geom_bar(aes(alpha = colors_var),
+      geom_bar(aes(alpha   = colors_var),
                stat = "identity",
-               position = "dodge",  # Change to "dodge" for grouped bars
-               show.legend = F) +
-      geom_text(aes(alpha = colors_var),
+               position = "dodge",
+               show.legend = FALSE) +
+      geom_text(aes(alpha   = colors_var),
                 position = position_dodge(width = 0.9),
                 vjust = -0.5,
                 size = 3.514598,
-                show.legend = F) +
+                show.legend = FALSE) +
       scale_alpha_manual(values = transparencies)
   }
   
@@ -57,30 +54,28 @@ LAC_groupedBarChart <- function(
     plt <- plt +
       geom_text_repel(mapping = aes(label = labels_var),
                       position = position_dodge(width = 0.9),
-                      family = "Lato Full",
-                      fontface = "bold",
-                      size = 3.514598,
-                      show.legend = F,
+                      family      = "Lato Full",
+                      fontface    = "bold",
+                      size        = 3.514598,
+                      show.legend = FALSE,
                       min.segment.length = 1000,
-                      seed = 42,
-                      box.padding = 0.5,
-                      direction = "y",
-                      force = 5,
-                      force_pull = 1)
+                      seed               = 42,
+                      box.padding        = 0.5,
+                      direction          = "y",
+                      force              = 5,
+                      force_pull         = 1)
   }
   
-  # Continuing with ggplot  
-  plt <- plt +
-    scale_fill_manual(values = colors) +
-    scale_y_continuous(limits = c(0, 105),
-                       expand = c(0, 0),
-                       breaks = seq(0, 100, 20),
-                       labels = paste0(seq(0, 100, 20), "%")) +
-    theme_minimal()  # Change to the theme you prefer
-  
-  if (custom.axis) {
+  if (custom.axis == FALSE) {
     plt <- plt +
-      scale_x_discrete(breaks = x.breaks, labels = x.labels)
+      scale_fill_manual(values = colors) +
+      theme_minimal()
+  } else {
+    plt <- plt +
+      scale_fill_manual(values = colors) +
+      scale_x_discrete(breaks = x.breaks,
+                       labels = x.labels) +
+      theme_minimal()
   }
   
   return(plt)
