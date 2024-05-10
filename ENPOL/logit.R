688c6211a9b8617e2278304235a3940282187533
CristinaA-Venzor <85714147+CristinaAVenzor@users.noreply.github.com>
2024-05-10 16:43:34 -0700

    Update logit.R

--- ENPOL/logit.R
+++ ENPOL/logit.R
@@ -23,7 +23,7 @@ logit_demo_panel <- function(mainData = data2plot,
                  size = point_size, position = position_dodge(width = .7), color = point_color) +
       geom_point(aes(x = reorder(factor, -order_variable), y = AME), 
                  size = 2, position = position_dodge(width = .7), color = "white") +
-      labs(y = "Menos probable                                  Más probable") +
+      labs(y = "Menos probable                              Más probable") +
       scale_y_continuous(limits = c(-0.20, 0.20),
                          breaks = seq(-0.18, 0.18, by = 0.09),
                          expand = expansion(mult = 0.025), position = "right",
@@ -56,7 +56,7 @@ logit_demo_panel <- function(mainData = data2plot,
                  size = point_size, position = position_dodge(width = .7), color = point_color) +
       geom_point(aes(x = reorder(factor, -order_variable), y = AME), 
                  size = 2, position = position_dodge(width = .7), color = "white") +
-      labs(y = "Menos probable                                  Más probable") +
+      labs(y = "Menos probable                          Más probable") +
       scale_y_continuous(limits = c(-0.20, 0.20),
                          breaks = seq(-0.18, 0.18, by = 0.09),
                          expand = expansion(mult = 0.025), position = "right",
@@ -78,43 +78,6 @@ logit_demo_panel <- function(mainData = data2plot,
             panel.grid.minor.y = element_blank())
     
   }
-
-  plot <- ggplot(mainData, aes(x = reorder(factor, -order_variable), y = AME)) +
-    geom_hline(yintercept = 0, lty = 1, color = "#fa4d57", lwd = 1)  +
-    geom_linerange(aes(x = reorder(factor, -order_variable),  ymin = lower, ymax = upper),
-                   lwd = line_size, position = position_dodge(width = .7), 
-                   stat = "identity", color = line_color)+
-    geom_point(aes(x = reorder(factor, -order_variable), y = AME), 
-               size = point_size, position = position_dodge(width = .7), color = point_color) +
-    geom_point(aes(x = reorder(factor, -order_variable), y = AME), 
-               size = 2, position = position_dodge(width = .7), color = "white") +
-
-    labs(y = "Menos probable                                  Más probable") +
-    scale_y_continuous(limits = c(-0.25, 0.25),
-                       breaks = seq(-0.25, 0.25, by = 0.125),
-                       expand = expansion(mult = 0.025), position = "right",
-                       labels = c("-15", "-7.5", "0", "+7.5", "+15"))+
-
-    labs(y = "Menos probable                               Más probable") +
-    scale_y_continuous(limits = c(-0.15, 0.15),
-                       breaks = seq(-0.12, 0.12, by = 0.06),
-                       expand = expansion(mult = 0.025), position = "right",
-                       labels = c("-10 p.p.", "-5 p.p.", "0", "+5 p.p.", "+10 p.p.")) +
-    WJP_theme()+
-    coord_flip() +
-    theme(legend.position = "none",
-          panel.background   = element_blank(),
-          panel.grid.major.x = element_line(colour = "#d1cfd1", 
-                                            size = 0.5, linetype = "dashed"),
-          legend.title = element_blank(),
-          axis.title.y       = element_blank(),
-          axis.text.y        = element_text(family = "Lato Medium",
-                                            size     = 3.514598*.pt,
-                                            color    = "#4a4a49",
-                                            hjust    = 0),
-          panel.grid.major.y = element_blank(),
-          panel.grid.minor.x  = element_blank(),
-          panel.grid.minor.y = element_blank())
 
   
   return(plot)
